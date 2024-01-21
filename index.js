62da83733ee862b1b70c21e5cd32a47e7ead3e34
AbdirahmanDiyatF <ibnudiyat273@gmail.com>
2024-01-21 08:22:53 +0300

    Arithmetic

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
 
 // Write your code here
+let multiply,num1,num2;
+num1=2;
+num2=31;
+multiply=num1*num2;
+let random;
+random = Math.floor(Math.random()*10);
+let mod, num3, num4;
+num3=25;
+num4=7;
+mod= num3%num4;
+let max;
+max = Math.max(4,11,6,20,15);
+
+
